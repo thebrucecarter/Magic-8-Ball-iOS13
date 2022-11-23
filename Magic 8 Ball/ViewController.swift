da6fb366c7a306538ead74c2731af4a552cf8372
Bruce Carter <thebrucecarter@gmail.com>
2022-11-23 16:44:50 -0500

    Changed ball array

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -12,8 +12,10 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var imageView: UIImageView!
     
-    let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
+//    let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
 
+    let ballArray = [UIImage(imageLiteralResourceName: "ball1.png"), UIImage(imageLiteralResourceName: "ball2.png"), UIImage(imageLiteralResourceName: "ball3.png"), UIImage(imageLiteralResourceName: "ball4.png"), UIImage(imageLiteralResourceName: "ball5.png")]
+    
     @IBAction func askButtonPressed(_ sender: UIButton) {
         
         imageView.image = ballArray.randomElement()
